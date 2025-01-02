512c35348f2f239c5c1bb6fc7fc5a31cd77b7410
霜月柚希 🍊 <yuzukishimotsuki-dev@lol-icon.app>
2025-01-02 10:04:34 +0800

    Update ip_info.py

--- src/ip_info.py
+++ src/ip_info.py
@@ -1,7 +1,7 @@
 """
 The MIT License (MIT)
 
-Copyright (c) 2024 ChocolaMilk92
+Copyright (c) 2024 Yuzuk1Shimotsuki
 
 Permission is hereby granted, free of charge, to any person obtaining a
 copy of this software and associated documentation files (the "Software"),
